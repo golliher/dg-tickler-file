238ca6468a12e1deb08f91626ee8f4e47ad7764f
Darrell Golliher <darrell@golliher.net>
2009-12-24 08:26:07 -0500

    First attempt at adding textinput dialog (fail).  Checkpoint before trying something different.

--- tickler-helper.py
+++ tickler-helper.py
@@ -5,6 +5,7 @@ import parsedatetime.parsedatetime_consts as pdc
 import sys
 import os
 import time
+import wx
 
 # create an instance of Constants class so we can override some of the defaults
 
@@ -14,18 +15,36 @@ c = pdc.Constants()
 
 p = pdt.Calendar(c)
 
-user_input = sys.stdin.readlines()
+# TODO: Check to see if there was user input, if not, then popup dialog
+# user_input = sys.stdin.readlines()
 
-try: 
-	# parse "tomorrow" and return the result
-	result = p.parse(user_input[0])
-	result = time.struct_time(result[0])
-	print result.tm_mon, result.tm_mday
-		
-except:
-	print "Could not parse your input.  Sorry"
+class TicklerWindow(wx.App):
+	def OnInit(self):
+		dlg = wx.TextEntryDialog(
+		        self, 'What date should this tickele?',
+		        'Eh??', 'Python')
 
+		dlg.SetValue("tomorrow")
 
-cmd = "open '/Users/golliher/Documents/Tickler File/%s/%s/%s'" %  (result.tm_year, result.tm_mon, result.tm_mday)
+		if dlg.ShowModal() == wx.ID_OK:
+		    self.log.WriteText('You entered: %s\n' % dlg.GetValue())
+		    user_input = dlg.GetValue()	
 
-os.system(cmd)
+		dlg.Destroy()
+
+app = TicklerWindow(0)
+app.MainLoop()
+
+###   try: 
+###   	# parse "tomorrow" and return the result
+###   	result = p.parse(user_input[0])
+###   	result = time.struct_time(result[0])
+###   	print result.tm_mon, result.tm_mday
+###   		
+###   except:
+###   	print "Could not parse your input.  Sorry"
+###   
+###   
+###   cmd = "open '/Users/golliher/Documents/Tickler File/%s/%s/%s'" %  (result.tm_year, result.tm_mon, result.tm_mday)
+###   
+###   os.system(cmd)
