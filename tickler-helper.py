b4df2c2ce0c3789e87ecd1848645893b116afb7f
Darrell Golliher <darrell@golliher.net>
2009-12-26 14:03:17 -0500

    Implemented a dialog box when the users input can not be parsed.

--- tickler-helper.py
+++ tickler-helper.py
@@ -2,42 +2,38 @@
 
 import parsedatetime.parsedatetime as pdt
 import parsedatetime.parsedatetime_consts as pdc
-import sys
-import os
-import time
-import wx
+import sys,os,time,wx
+
 
 # create an instance of Constants class so we can override some of the defaults
-
 c = pdc.Constants()
 
-# create an instance of the Calendar class and pass in our Constants # object instead of letting it create a default
-
+# create an instance of the Calendar class and pass in our Constants 
+# object instead of letting it create a default
 p = pdt.Calendar(c)
-
-
-# TODO: Check to see if there was user input, if not, then popup dialog
-# user_input = sys.stdin.readlines()
 
 app = wx.App()
 dlg = wx.TextEntryDialog(
         None, 'What date should this tickle?',
         '', 'tomorrow')
 
-#dlg.SetValue("tomorrow")
-
 if dlg.ShowModal() == wx.ID_OK:
-    user_input = dlg.GetValue()	
+    user_input = dlg.GetValue()
 
     try: 
-        # parse "tomorrow" and return the result
+        # parse "tomorrow" and return the result as a tuple
         result = p.parse(user_input)
+        if result[1] == 0:
+             raise Exception("Not able to parse user input")
+
         result = time.struct_time(result[0])
         cmd = "open '/Users/golliher/Documents/Tickler File/%s/%s/%s'" %  (result.tm_year, result.tm_mon, result.tm_mday)
         os.system(cmd)
 
     except:
-        print "Could not parse your input.  Sorry"
+        wx.MessageBox('Didn\'t understand what you meant by "%s"' % user_input,'Sorry...')
+ 
+        
 
 dlg.Destroy()
 app.MainLoop()
