7b741650c649b594330318477d2227201a83c10a
Darrell Golliher <darrell@golliher.net>
2010-01-03 20:48:00 -0500

    Added rudimentary support for KDE and GNOME.  Linux users will need to set a config option for konqueor or nautilus because I've not come up with a way of detecting which to use automatically.
    See sample_config.ini for example.

--- open_folder.py
+++ open_folder.py
@@ -1,4 +1,9 @@
 import os, platform
+import ConfigParser
+
+config = ConfigParser.ConfigParser()
+config.read("config.ini")
+
 
 # I intend to hide the Operating Specific details of opening a folder
 #   here in this module.  
@@ -12,8 +17,10 @@ import os, platform
 def open_folder(path):
     path = os.path.normpath(path)
     cmd = {'Darwin': 'open',      # aka. Mac OS X
+           'Linux':  config.get('linux','file_explorer'), 
            'Windows':'explorer'}[platform.system()]
     format_string = {'Darwin': "%s '%s'",  # note the quotation marks around path
+                     'Linux':  "%s '%s'", # BUG:  not all Linux distros use nautilus
                      'Windows': "%s %s"}[platform.system()]
     os.popen(format_string % (cmd, path))
     
