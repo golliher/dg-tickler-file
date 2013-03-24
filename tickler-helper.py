64f057ec66d533ecb1d9dda5b5f0c09a3649f72b
Darrell Golliher <darrell@golliher.net>
2013-03-23 20:47:30 -0400

    Version that works with and without wxPython.

--- tickler-helper.py
+++ tickler-helper.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python 
+#!/usr/bin/env python
 #
 # Program that make is easy for user to pop open a folder for a specific date.
 #   exampleS:   "next tuesday"  or "20 days"  or "1/1/2011"
@@ -8,11 +8,6 @@
 import parsedatetime.parsedatetime as pdt
 import parsedatetime.parsedatetime_consts as pdc
 import sys,os,time
-try:
-    import wx
-except ImportError
-    pass
-    
 import ConfigParser
 from open_folder import open_folder
 
@@ -26,29 +21,44 @@ c = pdc.Constants()
 # object instead of letting it create a default
 p = pdt.Calendar(c)
 
-app = wx.App()
-dlg = wx.TextEntryDialog(None, 'What date should this tickle?','', 'tomorrow')
-
-if dlg.ShowModal() == wx.ID_OK:
-    user_input = dlg.GetValue()
-    try: 
-        # parse "tomorrow" and return the result as a tuple
-        result = p.parse(user_input)
-        if result[1] == 0:
-             raise Exception("Not able to parse user input")
-
-        result = time.struct_time(result[0])
-        folder_name = '{0}/{1}/{2:02}/{3:02}'.format(
-                                            config.get("global","tickle_file_dir"),
-                                            result.tm_year, 
-                                            result.tm_mon, 
-                                            result.tm_mday)
-        open_folder(folder_name)
+    
+if len(sys.argv) >= 2:
+    # wx.MessageBox("CLI INPUT DETECTED %s" % sys.argv[1])
+    user_input = sys.argv[1]
         
+else:
+    try:
+        import wx
+        app = wx.App()
     except:
-        wx.MessageBox('Didn\'t understand what you meant by "%s"' 
-                                            % user_input,'Sorry...')
+        print "Unless you specify arguments on the command line wxpython is required."
+        print "wxpython not able to be imported. exiting."
+        sys.exit(1)
 
-dlg.Destroy()
-app.MainLoop()
+    dlg = wx.TextEntryDialog(None, 'What date should this tickle?','', 'tomorrow')
+    if dlg.ShowModal() == wx.ID_OK:
+        user_input = dlg.GetValue()
+    dlg.Destroy()
+    
+try: 
+    # parse "tomorrow" and return the result as a tuple
+    result = p.parse(user_input)
+    if result[1] == 0:
+         raise Exception("Not able to parse user input")
+
+    result = time.struct_time(result[0])
+    folder_name = '{0}/{1}/{2:02}/{3:02}'.format(
+                                        config.get("global","tickle_file_dir"),
+                                        result.tm_year, 
+                                        result.tm_mon, 
+                                        result.tm_mday)
+    open_folder(folder_name)
+    
+except:
+    wx.MessageBox('Didn\'t understand what you meant by "%s"' 
+                                        % user_input,'Sorry...')
+
+if 'app' in globals():
+    app.MainLoop()
+
 
