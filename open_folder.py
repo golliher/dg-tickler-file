6550091bd178223e7b134abc6a84fc0a79f3cd87
Darrell Golliher <darrell@golliher.net>
2015-07-18 06:48:30 -0400

    Removed configuration from open_folder
    xdg-open for linux makes configuration unnecessary.  Allows simpler version of this function.

--- open_folder.py
+++ open_folder.py
@@ -1,13 +1,7 @@
 import os, platform
-import ConfigParser
-
-config = ConfigParser.ConfigParser()
-config.read("config.ini")
-
-
 # I intend to hide the Operating Specific details of opening a folder
-#   here in this module.  
-# 
+#   here in this module.
+#
 # On Mac OS X you do this with "open"
 #   e.g. "open '\Users\golliher\Documents\Tickler File'"
 # On Windows you do this with "explorer"
@@ -16,11 +10,9 @@ config.read("config.ini")
 
 def open_folder(path):
     path = os.path.normpath(path)
-    cmd = {'Darwin': 'open',      # aka. Mac OS X
-           'Linux':  config.get('linux','file_explorer'), 
-           'Windows':'explorer'}[platform.system()]
-    format_string = {'Darwin': "%s '%s'",  # note the quotation marks around path
-                     'Linux':  "%s '%s'", # BUG:  not all Linux distros use nautilus
-                     'Windows': "%s %s"}[platform.system()]
-    os.popen(format_string % (cmd, path))
-    
+
+    format_string = {'Darwin': "open '%s'",  # note the quotation marks around path
+                     'Linux':  "xdg-open '%s'",
+                     'Windows': "explorer %s"}[platform.system()]
+
+    os.popen(format_string % path)
