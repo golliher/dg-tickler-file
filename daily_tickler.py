5b8c76ba4e5dbe404d90351eaf3af12bc7d0728d
Darrell Golliher <darrell@golliher.net>
2015-07-18 08:45:30 -0400

    Moved growl to function.  Cleaned up comments.

--- daily_tickler.py
+++ daily_tickler.py
@@ -11,7 +11,6 @@ from open_folder import *
 
 import gntp.notifier
 
-#image = open('./folder-icon-512x512.png', 'rb').read()
 image = open('./myfolder.png', 'rb').read()
 
 # More complete example
@@ -21,6 +20,18 @@ growl = gntp.notifier.GrowlNotifier(
     defaultNotifications = ["New Messages"],
 )
 growl.register()
+
+
+def notify_user(msg):
+    growl.notify(
+        noteType = "New Messages",
+        title = "Checked today's ticker file.",
+        description = msg,
+        icon = image,
+
+        sticky = True,
+        priority = 1,
+    )
 
 
 
@@ -33,7 +44,7 @@ today = datetime.date.today()
 todays_folder = config.get("global","tickle_file_dir") + "/" + today.strftime("%Y/%m/%d")
 
 
-    
+
 
 the_today_folder = config.get("global","the_today_folder")
 
@@ -44,7 +55,7 @@ except:
     print("Warning: Unabled to remove old symlink.")
 
 # Create a symlink to make it easy for user to open up "today"
-# (NOTE TO SELF: Perhaps windows users just don't get this convenience? 
+# (NOTE TO SELF: Perhaps windows users just don't get this convenience?
 #   (because no symlinks on windows)
 cmd = 'ln -s "%s" "%s"' % (todays_folder,the_today_folder)
 try:
@@ -57,17 +68,7 @@ file_count = len( [name for name in os.listdir(todays_folder) ]   )
 
 if (file_count == 0):
     msg = "Today's tickler file was not opened because it contained no files. \n\n%s" % datetime.datetime.fromtimestamp(time.time()).strftime('%Y-%m-%d %H:%M:%S')
-    growl.notify(
-        noteType = "New Messages",
-        title = "Checked today's ticker file.",
-        description = msg,
-        # icon = "http://www.graphicsfuel.com/wp-content/uploads/2012/03/folder-icon-512x512.png",
-        # icon = "http://www.theofficedealer.com/mm5/graphics/product_images/1300/seo/Pendaflex-9200T-1-3-Pressboard-File-Folder.jpg",
-        icon = image,
-
-        sticky = True,
-        priority = 1,
-    )
+    notify_user(msg)
 else:
-    # Go ahead and pop open the folder to it's front and center
+    # Go ahead and pop open the folder so it's front and center
     open_folder(todays_folder)
