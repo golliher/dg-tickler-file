2b8208e2f6ba8554aefc8e984132a0d4084c26ec
Darrell Golliher <darrell@golliher.net>
2015-07-18 08:21:38 -0400

    Raise exception if path not found, os not found, or command execution fails.

--- open_folder.py
+++ open_folder.py
@@ -1,4 +1,4 @@
-import os, platform
+import os, platform, subprocess
 # I intend to hide the Operating Specific details of opening a folder
 #   here in this module.
 #
@@ -13,14 +13,19 @@ def open_folder(path):
 
     path = os.path.normpath(path)
 
+    if not os.path.isdir(path):
+        raise Exception("Folder does not exist.")
+
+    # Find the operating system command to open a folder
     try:
-        platform_cmd_formatstring = { 'Darwin':  "open '%s'",  # note the quotation marks around path
-                                      'Linux':   "xdg-open '%s'",
-                                      'Windows': "explorer %s"}[platform.system()]
+        platform_cmd = {    'Darwin':  "open",  # note the quotation marks around path
+                            'Linux':   "xdg-open",
+                            'Windows': "explorer"}[platform.system()]
     except:
         raise Exception("Your operating system was not recognized.  Unable to determine how to open folders for you.")
 
-
-    platform_cmd = platform_cmd_formatstring % path
-
-    os.popen(platform_cmd)
+    # Run the operating system command to open the folder
+    try:
+         subprocess.check_call([platform_cmd,path])
+    except OSError, e:
+        raise Exception("Failed attempt executing folder opening command for your OS. \nCMD: %s\nARG: %s\nRESULT: %s\n" % (platform_cmd, path, e.strerror) )
