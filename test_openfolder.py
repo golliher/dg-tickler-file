6977ac059e57ccf9e79807431900f5a7584f6a83
Darrell Golliher <darrell@golliher.net>
2015-07-21 15:47:00 -0400

    open_folder() has no return value so I now test for that.

--- test_openfolder.py
+++ test_openfolder.py
@@ -5,7 +5,8 @@ from open_folder import *
 
 def test_folder_exists():
     with patch('subprocess.check_call', MagicMock(return_value="NOOP")):
-        open_folder(".")
+        result = open_folder(".")
+        assert result == None
 
 def test_folder_does_not_exists():
     with patch('subprocess.check_call', MagicMock(return_value="NOOP")):
@@ -16,14 +17,17 @@ def test_unsupported_os():
     with patch('subprocess.check_call', MagicMock(return_value="NOOP")):
         with patch('platform.system', MagicMock(return_value="NotDarwinWindowsLinux")):
             with pytest.raises(Exception):
-                open_folder("/")
+                result = open_folder("/")
 
 def test_supported_os():
     with patch('subprocess.check_call', MagicMock(return_value="NOOP")):
 
         with patch('platform.system', MagicMock(return_value="Linux")):
-            open_folder("/")
+            result = open_folder("/")
+            assert result == None
         with patch('platform.system', MagicMock(return_value="Darwin")):
-            open_folder("/")
+            result = open_folder("/")
+            assert result == None
         with patch('platform.system', MagicMock(return_value="Windows")):
-            open_folder("/")
+            result = open_folder("/")
+            assert result == None
